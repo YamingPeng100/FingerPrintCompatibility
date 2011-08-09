a7d7e411514898d72a7f1a6f209d8a21ad2f9d51
rthieblin <remi.thieblin@gmail.com>
2011-08-09 15:45:03 +0200

    id admob ajouté

--- src/com/actimust/seduction/Resultat.java
+++ src/com/actimust/seduction/Resultat.java
@@ -1,7 +1,6 @@
 package com.actimust.seduction;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Window;
